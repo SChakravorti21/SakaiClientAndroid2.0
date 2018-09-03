6e3d88f6071ba1945cdc0a4f8f54a6fce892481b
SChakravorti21 <shoumyoarchit@gmail.com>
2018-09-03 09:37:35 -0400

    Removed over-engineered converters and Gson POJOs for assignments

--- app/src/main/java/com/example/development/sakaiclient20/models/sakai/assignments/AssignmentsResponse.java
+++ app/src/main/java/com/example/development/sakaiclient20/models/sakai/assignments/AssignmentsResponse.java
@@ -1,5 +1,6 @@
 package com.example.development.sakaiclient20.models.sakai.assignments;
 
+import com.example.development.sakaiclient20.persistence.entities.Assignment;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -12,7 +13,7 @@ public class AssignmentsResponse implements Serializable
 
     @SerializedName("assignment_collection")
     @Expose
-    private List<Assignment> assignment = new ArrayList<Assignment>();
+    private List<Assignment> assignment = new ArrayList<>();
     private final static long serialVersionUID = 6209006925278274013L;
 
     public List<Assignment> getAssignments() {
